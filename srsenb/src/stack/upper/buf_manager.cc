81f6dde18e772288335bb685c2ebfb0812c9c06e
Qianru Li <qianru@Qianrus-MacBook-Pro.local>
2021-02-27 08:52:42 -0800

    debug

--- srsenb/src/stack/upper/buf_manager.cc
+++ srsenb/src/stack/upper/buf_manager.cc
@@ -58,11 +58,11 @@ bool gtpu_buffer_manager::check_space_new_sdu(uint16_t rnti)
 {
   if (buffer_map.size() > 0) {
     uint32_t max_rnti = buffer_map.begin()->first;
-    for (auto it = buffer_map.begin(); it != buffer_map.end(); ++it ){
+    for (user_buffer_state_map_t::iterator it = buffer_map.begin(); it != buffer_map.end(); ++it ){
+      buf_log->info("[buf-debug] Iterate user rnti=%u, nof_packets=%u\n", it->first, it->second.get_user_nof_packets());
       if (it->second.get_user_nof_packets() > buffer_map.at(max_rnti).get_user_nof_packets())
         max_rnti = it->first;
-    }
-    buf_log->info("[buf-debug] Most busy user rnti=%u, nof_packets=%u\n", max_rnti, buffer_map.at(max_rnti).get_user_nof_packets());
+    } 
   }
 	return true;
 	// if (nof_packets >= BUF_CAPACITY_PKT) {
@@ -103,7 +103,7 @@ void user_buffer_state::update_buffer_state(uint32_t lcid, uint32_t nof_unread_p
   user_nof_bytes += nof_unread_bytes;
   if (user_buffer_map.count(lcid)) {
   	user_nof_packets -= user_buffer_map[lcid].first;
-  	user_nof_bytes -= (nof_unread_bytes - user_buffer_map[lcid].second);
+  	user_nof_bytes -= user_buffer_map[lcid].second;
   }
   user_buffer_map[lcid] = std::make_pair(nof_unread_packets, nof_unread_bytes);
   pthread_mutex_unlock(&mutex);
