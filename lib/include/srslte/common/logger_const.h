05255cfded116dd93ddbfa02b5266267843ec11a
Qianru Li <qianru@Qianrus-MacBook-Pro.local>
2021-04-02 21:51:01 -0700

    continue

--- lib/include/srslte/common/logger_const.h
+++ lib/include/srslte/common/logger_const.h
@@ -8,11 +8,7 @@
 namespace srslte {
 
 typedef enum {
-  LTE_PHY_PUSCH_Decoding_Result = 0xB139,
-  LTE_PHY_PUCCH_Decoding_Result = 0xB13C,
-  LTE_MAC_DL_Transport_Block    = 0xB063,
-  LTE_MAC_DL_Stats      = 0xF001,
-  LTE_MAC_UL_Stats      = 0xF002,
+  LTE_RRC_OTA_Packet = 0xB0C0,
   LTE_GTPU_Buffer_Stats = 0xF003,
   LTE_PDCP_DL_Data_PDU  = 0xB0A3,
   LTE_PDCP_UL_Data_PDU  = 0xB0B3,
@@ -20,17 +16,18 @@ typedef enum {
   LTE_RLC_DL_AM_All_PDU = 0xB082,
   LTE_RLC_UL_UM_All_PDU = 0xB093,
   LTE_RLC_DL_UM_All_PDU = 0xB083,
+  LTE_MAC_DL_Transport_Block    = 0xB063,
 } MiMessageType; 
 
   // LTE_RLC_UL_Stats      = 0xB097;
   // LTE_RLC_DL_Stats      = 0xB087;
+  // LTE_PHY_PUSCH_Decoding_Result = 0xB139,
+  // LTE_PHY_PUCCH_Decoding_Result = 0xB13C,
+  // LTE_MAC_DL_Stats      = 0xF001,
+  // LTE_MAC_UL_Stats      = 0xF002,
 
 const std::unordered_map<std::string, MiMessageType> MsgTypeToName ( {
-  {"LTE_PHY_PUSCH_Decoding_Result", LTE_PHY_PUSCH_Decoding_Result},
-  {"LTE_PHY_PUCCH_Decoding_Result", LTE_PHY_PUCCH_Decoding_Result},
-  {"LTE_MAC_DL_Transport_Block", LTE_MAC_DL_Transport_Block},
-  {"LTE_MAC_DL_Stats", LTE_MAC_DL_Stats},
-  {"LTE_MAC_UL_Stats", LTE_MAC_UL_Stats},
+  {"LTE_RRC_OTA_Packet", LTE_RRC_OTA_Packet},
   {"LTE_GTPU_Buffer_Stats", LTE_GTPU_Buffer_Stats},
   {"LTE_PDCP_DL_Data_PDU", LTE_PDCP_DL_Data_PDU},
   {"LTE_PDCP_UL_Data_PDU", LTE_PDCP_UL_Data_PDU},
@@ -38,9 +35,15 @@ const std::unordered_map<std::string, MiMessageType> MsgTypeToName ( {
   {"LTE_RLC_DL_AM_All_PDU", LTE_RLC_DL_AM_All_PDU},
   {"LTE_RLC_UL_UM_All_PDU", LTE_RLC_UL_UM_All_PDU},
   {"LTE_RLC_DL_UM_All_PDU", LTE_RLC_DL_UM_All_PDU},
+  {"LTE_MAC_DL_Transport_Block", LTE_MAC_DL_Transport_Block},
+} );
+
   // {"LTE_RLC_UL_Stats", LTE_RLC_UL_Stats},
   // {"LTE_RLC_DL_Stats", LTE_RLC_DL_Stats},
-} );
+  // {"LTE_MAC_DL_Stats", LTE_MAC_DL_Stats},
+  // {"LTE_MAC_UL_Stats", LTE_MAC_UL_Stats},
+  // {"LTE_PHY_PUSCH_Decoding_Result", LTE_PHY_PUSCH_Decoding_Result},
+  // {"LTE_PHY_PUCCH_Decoding_Result", LTE_PHY_PUCCH_Decoding_Result},
 
 } // namespace srslte
 
